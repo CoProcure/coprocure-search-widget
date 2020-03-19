214afb0e62c1161aec544df485002a29237a0721
Mark Rossetti <mark@coprocure.us>
2020-03-19 16:52:29 -0700

    Add new buyer and coop to filters
    Buyer: Eagle County Health Service District
    Coop: Savvik Buying Group

--- src/search/coops.js
+++ src/search/coops.js
@@ -1,9 +1,19 @@
 export function coops() {
-  return ["1Government Procurement Alliance (1GPA)", "BuyBoard", "Choice Partners", "E&I Cooperative Services", "ESC19 Allied States Cooperative", "HGACBuy", "Kansas City Regional Purchasing Cooperative (KCRPC)", "Minnesota Multistate Contracting Alliance for Pharmacy (MMCAP)", "NASPO ValuePoint", "National Cooperative Purchasing Alliance (NCPA)", "OMNIA Partners", "Sourcewell", "The Interlocal Purchasing System (TIPS)", "Texas Department of Information Resources (DIR)"]
+  return [
+    "1Government Procurement Alliance (1GPA)",
+    "BuyBoard",
+    "Choice Partners",
+    "E&I Cooperative Services",
+    "ESC19 Allied States Cooperative",
+    "HGACBuy",
+    "Kansas City Regional Purchasing Cooperative (KCRPC)",
+    "Minnesota Multistate Contracting Alliance for Pharmacy (MMCAP)",
+    "NASPO ValuePoint",
+    "National Cooperative Purchasing Alliance (NCPA)",
+    "OMNIA Partners",
+    "Savvik Buying Group",
+    "Sourcewell",
+    "The Interlocal Purchasing System (TIPS)",
+    "Texas Department of Information Resources (DIR)"
+  ];
 }
-
-
-
-
-
-
